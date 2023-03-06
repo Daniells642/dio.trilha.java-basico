1aef03d448ba14cbddd8e778b31e32cfe1e3909f
Daniel <daniells642@hotmail.com>
2023-03-06 11:26:35 -0300

    aula de operadores iniciada

---
+++ java-basico/src/edu/daniel/operadores/Operadores.java
@@ -0,0 +1,10 @@
+package edu.daniel.operadores;
+
+import java.util.Date;
+
+public class Operadores {
+    public static void main(String[] args) {
+        Date dataNascimento = new Date();
+        System.out.println(dataNascimento);
+    }
+}
---
+++ java-basico/src/edu/daniel/tipos-variaveis/TiposVariaveis.java
@@ -0,0 +1,13 @@
+public class TiposVariaveis{
+    public static void main(String[] args) throws Exception {
+
+        // double salarioMinimo = 2500;
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        // final double PI = 3.14;
+        
+        System.out.println(numeroCurto2);
+    }
+}
