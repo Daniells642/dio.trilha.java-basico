dc689acdf285cb78786a41467019ecb09525b697
Daniel <daniells642@hotmail.com>
2023-03-07 14:01:06 -0300

    update

--- java-basico/src/edu/daniel/operadores/Operadores.java
+++ java-basico/src/edu/daniel/operadores/Operadores.java
@@ -5,6 +5,6 @@ import java.util.Date;
 public class Operadores {
     public static void main(String[] args) {
         Date dataNascimento = new Date();
-        System.out.println(dataNascimento);
+        System.out.println("Hora certa:" + dataNascimento);
     }
 }
